de9b86179f0bbccb466228ea44e069b48d01e458
Mert <mertparlak123@hotmail.com>
2024-12-14 14:06:17 +0300

    add @tanstack/react-query and @tanstack/react-query-devtools for data fetching and state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Bookings from "./pages/Bookings";
 import Cabins from "./pages/Cabins";
 import Users from "./pages/Users";
@@ -10,9 +12,18 @@ import GloablStyles from "./styles/GlobalStyles";
 import Dashboard from "./pages/Dashboard";
 import AppLayout from "./ui/AppLayout";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GloablStyles />
       <BrowserRouter>
         <Routes>
@@ -29,7 +40,7 @@ function App() {
           <Route path="*" element={<PageNotFount />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 }
 
