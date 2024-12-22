e3cb77f0cc311cd34c7a5e5bf00669cc2e7f25b6
Mert <mertparlak123@hotmail.com>
2024-12-22 14:05:31 +0300

    refactor CabinTable to use custom hook for fetching cabins

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { getCabins } from "../../services/apiCabins";
 import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
+import { useGetCabins } from "../../hooks/useGetCabins";
 
 const Table = styled.div`
   border: 1px solid var(--color-grey-200);
@@ -29,15 +30,7 @@ const TableHeader = styled.header`
 `;
 
 export default function CabinTable() {
-  const {
-    isPending,
-    data: cabins,
-    error,
-  } = useQuery({
-    queryKey: ["cabins"],
-    queryFn: getCabins,
-  });
-
+  const { isPending, cabins } = useGetCabins();
   if (isPending) return <Spinner />;
 
   if (cabins) {
