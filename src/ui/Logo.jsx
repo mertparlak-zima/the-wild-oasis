f0e0788da262ed90c710c5d6f29f108e166ee978
Mert <mertparlak123@hotmail.com>
2024-12-29 15:11:02 +0300

    wrap logo in NavLink for improved navigation

--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 const StyledLogo = styled.div`
@@ -11,9 +12,11 @@ const Img = styled.img`
 
 function Logo() {
   return (
-    <StyledLogo>
-      <Img src="/logo-light.png" alt="Logo" />
-    </StyledLogo>
+    <NavLink to="/">
+      <StyledLogo>
+        <Img src="/logo-light.png" alt="Logo" />
+      </StyledLogo>
+    </NavLink>
   );
 }
 
